582343a526994ef67b73a00d0fe727c2d306024d
Shubham <shubhamk.jaiswal1919@gmail.com>
2021-08-26 22:37:01 +0530

    API

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,16 +1,18 @@
 import React from "react";
-const Pagination = ({ postPerPage, totalPosts, Paginate  }) => {
-
+const Pagination = ({ postPerPage, totalPosts, Paginate }) => {
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalPosts / postPerPage); i++) {
     pageNumbers.push(i);
   }
   return (
-
     <nav>
       <ul className="pagination">
         {pageNumbers.map((number) => (
-          <li key={number} className="page-item">
+          <li
+            style={{ display: "flex", flexDirection: "row" }}
+            key={number}
+            className="page-item"
+          >
             <a onClick={() => Paginate(number)} href="!#" className="page-link">
               {number}
             </a>
